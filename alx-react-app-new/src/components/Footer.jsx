589ad0ae3fbe6d1a97f7f2fa49f9c55b6ed90bac
pgkp-cmd <gichuhipeter32@gmail.com>
2025-02-23 23:53:59 +0300

    Update Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '10px' }}>
+      <p>© 2025 My Favorite Cities</p>
+    </footer>
+  );
+}
+
+export default Footer;
